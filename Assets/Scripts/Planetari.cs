b6f38cb18b677a3387baa7cf8bb463f4e8579666
Lluis Lazaro <lluis.lazaro@enti.cat>
2025-03-12 19:12:23 +0100

    Mapa Creat amb materials i tot i dos scripts
    Un es el de orbitacio de classe i l'altre es el que haurem de completar

---
+++ Assets/Scripts/Planetari.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planetari : MonoBehaviour
+{
+    public Vector2 initialVelocityMercuri;
+    public Vector2 initialVelocityVenus;
+    public Vector2 initialVelocityEarth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
