977c03f319c3b79ed28c603f429844ab2075c0f2
Lluis Lazaro <lluis.lazaro@enti.cat>
2025-03-31 21:10:28 +0200

    Trail Renderer fet

--- Assets/Scripts/Planetari.cs
+++ Assets/Scripts/Planetari.cs
@@ -4,52 +4,65 @@ using UnityEngine;
 
 public class Planetari : MonoBehaviour
 {
-	public Vector3 planetPosition;
+    public Vector3 planetPosition;
     public Vector3 planetVelocity;
 
-	public float Mass; // Massa realista (en unitats relatives al Sol)
+    public float Mass; // Massa realista (en unitats relatives al Sol)
+    private TrailRenderer trailRenderer;
+    public float stepTime = 0.0001f;
+    private float time = 0;
 
-	public float stepTime = 0.0001f;
-	private float time = 0;
+    private Vector3 netAcceleration;
+    private bool hasActivatedTrail = false;
 
-	private Vector3 netAcceleration;
+    private void Start()
+    {
+        trailRenderer = GetComponent<TrailRenderer>();
+        trailRenderer.enabled = false;
+    }
 
-	public void Initialize(Vector3 initialPosition, Vector3 initialVelocity)
-	{
-		planetPosition = initialPosition;
-		planetVelocity = initialVelocity;
-		transform.position = planetPosition * 10f; // escala per visualització
-	}
+    public void Initialize(Vector3 initialPosition, Vector3 initialVelocity)
+    {
+        planetPosition = initialPosition;
+        planetVelocity = initialVelocity;
+        transform.position = planetPosition * 10f; // escala per visualització
+    }
 
-	public void SetAcceleration(Vector3 acc)
-	{
-		netAcceleration = acc;
-	}
+    public void SetAcceleration(Vector3 acc)
+    {
+        netAcceleration = acc;
+    }
 
-	public void SimulateStep()
-	{
-		(planetPosition, planetVelocity, time) = RungeKutta4(planetPosition, planetVelocity, time);
-		transform.position = planetPosition * 10f;
-	}
+    public void SimulateStep()
+    {
+        (planetPosition, planetVelocity, time) = RungeKutta4(planetPosition, planetVelocity, time);
+        transform.position = planetPosition * 10f;
+    }
 
-	(Vector3, Vector3, float) RungeKutta4(Vector3 position, Vector3 velocity, float time)
-	{
-		Vector3 K1position, K1velocity, K2position, K2velocity, K3position, K3velocity, K4position, K4velocity;
+    (Vector3, Vector3, float) RungeKutta4(Vector3 position, Vector3 velocity, float time)
+    {
+        Vector3 K1position, K1velocity, K2position, K2velocity, K3position, K3velocity, K4position, K4velocity;
 
-		(K1position, K1velocity) = DifferentialFunction(position, velocity, netAcceleration);
-		(K2position, K2velocity) = DifferentialFunction(position + 0.5f * stepTime * K1position, velocity + 0.5f * stepTime * K1velocity, netAcceleration);
-		(K3position, K3velocity) = DifferentialFunction(position + 0.5f * stepTime * K2position, velocity + 0.5f * stepTime * K2velocity, netAcceleration);
-		(K4position, K4velocity) = DifferentialFunction(position + stepTime * K3position, velocity + stepTime * K3velocity, netAcceleration);
+        (K1position, K1velocity) = DifferentialFunction(position, velocity, netAcceleration);
+        (K2position, K2velocity) = DifferentialFunction(position + 0.5f * stepTime * K1position, velocity + 0.5f * stepTime * K1velocity, netAcceleration);
+        (K3position, K3velocity) = DifferentialFunction(position + 0.5f * stepTime * K2position, velocity + 0.5f * stepTime * K2velocity, netAcceleration);
+        (K4position, K4velocity) = DifferentialFunction(position + stepTime * K3position, velocity + stepTime * K3velocity, netAcceleration);
 
-		Vector3 newPosition = position + (stepTime / 6f) * (K1position + 2f * K2position + 2f * K3position + K4position);
-		Vector3 newVelocity = velocity + (stepTime / 6f) * (K1velocity + 2f * K2velocity + 2f * K3velocity + K4velocity);
-		float newTime = time + stepTime;
+        Vector3 newPosition = position + (stepTime / 6f) * (K1position + 2f * K2position + 2f * K3position + K4position);
+        Vector3 newVelocity = velocity + (stepTime / 6f) * (K1velocity + 2f * K2velocity + 2f * K3velocity + K4velocity);
+        float newTime = time + stepTime;
 
-		return (newPosition, newVelocity, newTime);
-	}
+        if (!hasActivatedTrail)
+        {
+            trailRenderer.enabled = true;
+            hasActivatedTrail = true;
+        }
 
-	(Vector3, Vector3) DifferentialFunction(Vector3 position, Vector3 velocity, Vector3 acceleration)
-	{
-		return (velocity, acceleration);
-	}
+        return (newPosition, newVelocity, newTime);
+    }
+
+    (Vector3, Vector3) DifferentialFunction(Vector3 position, Vector3 velocity, Vector3 acceleration)
+    {
+        return (velocity, acceleration);
+    }
 }
